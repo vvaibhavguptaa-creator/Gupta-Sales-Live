3d09957c541aea30ef560a606ac1181be0415a52
Gupta Admin <vvaibhavguptaa@gmail.com>
2026-02-07 13:06:06 +0530

    Fixed tsconfig and App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import Navbar from './components/Navbar';
-
 function App() {
   return (
-    <div className="bg-black min-h-screen text-white flex flex-col items-center justify-center">
-      <Navbar />
-      <h1 className="text-4xl font-serif text-yellow-500 mt-20">
-        SYSTEM ONLINE
-      </h1>
-      <p className="text-gray-400 mt-4">
-        The brain is connected. Ready to load Luxury Modules.
-      </p>
+    <div style={{
+      backgroundColor: 'red',
+      height: '100vh',
+      width: '100vw',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      color: 'white',
+      fontSize: '30px'
+    }}>
+      <h1>SYSTEM ONLINE</h1>
     </div>
   );
 }
