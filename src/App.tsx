bb0a11de54b521eef615761560f5ab97b97346d6
Gupta Admin <vvaibhavguptaa@gmail.com>
2026-02-07 13:15:28 +0530

    Back to Red Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Manifesto from './components/Manifesto';
-import ProductShowcase from './components/ProductShowcase';
-import ServicesBento from './components/ServicesBento';
-
 function App() {
   return (
-    <div className="bg-[#050505] min-h-screen w-full overflow-x-hidden text-white">
-
-      <Navbar />
-
-      {/* DEBUG MODE: We commented these out to see which one is the killer */}
-      <div className="pt-32 text-center">
-        <h1 className="text-4xl text-yellow-500 font-serif">SAFE MODE ACTIVE</h1>
-        <p className="text-gray-400 mt-4">If you see this, the App is healthy.</p>
-
-        {/* <Hero /> */}
-        {/* <Manifesto /> */}
-        {/* <ProductShowcase /> */}
-        {/* <ServicesBento /> */}
-      </div>
-
+    <div style={{
+      backgroundColor: 'red',
+      height: '100vh',
+      width: '100vw',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      color: 'white',
+      fontSize: '30px'
+    }}>
+      <h1>SYSTEM ONLINE</h1>
     </div>
   );
 }
