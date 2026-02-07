70e153239018441ed1f136ec9d37991dcde1bc4c
Gupta Admin <vvaibhavguptaa@gmail.com>
2026-02-07 13:17:37 +0530

    Testing Navbar Only

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <div style={{
-      backgroundColor: 'red',
-      height: '100vh',
-      width: '100vw',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      color: 'white',
-      fontSize: '30px'
-    }}>
-      <h1>SYSTEM ONLINE</h1>
+    <div className="bg-black min-h-screen w-full text-white">
+
+      {/* 1. If this works, you will see the Menu Bar at the top */}
+      <Navbar />
+
+      <div className="flex h-screen items-center justify-center flex-col">
+        <h1 className="text-4xl text-yellow-500 font-serif">TEST 1: NAVBAR PASSED</h1>
+        <p className="text-gray-500 mt-4">If you see this, the Icons are fixed.</p>
+      </div>
+
     </div>
   );
 }
