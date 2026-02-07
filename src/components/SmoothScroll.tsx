b52cce6d12d7c8bde4d9fc0e26c69e6fada9c173
Gupta Admin <vvaibhavguptaa@gmail.com>
2026-02-07 15:02:03 +0530

    scroll

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,3 +1,4 @@
+
 import { useEffect } from 'react';
 import Lenis from '@studio-freight/lenis';
 import gsap from 'gsap';
@@ -6,26 +7,26 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 const SmoothScroll = () => {
     useEffect(() => {
         const lenis = new Lenis({
-            duration: 1.5,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            duration: 1.2,
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Exponential easing
+            orientation: 'vertical',
+            gestureOrientation: 'vertical',
+            smoothWheel: true,
+            wheelMultiplier: 1,
             touchMultiplier: 2,
-            lerp: 0.1, // User specific request for "heavy" feel
         });
 
         lenis.on('scroll', ScrollTrigger.update);
 
-        const update = (time: number) => {
+        gsap.ticker.add((time) => {
             lenis.raf(time * 1000);
-        };
+        });
 
-        gsap.ticker.add(update);
-
-        // Disable lag smoothing to prevent jumps
         gsap.ticker.lagSmoothing(0);
 
         return () => {
             lenis.destroy();
-            gsap.ticker.remove(update);
+            gsap.ticker.remove(lenis.raf);
         };
     }, []);
 
