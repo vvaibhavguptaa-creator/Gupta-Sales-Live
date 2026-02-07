4725452e9506112dead27e8d55b6efe1e6126950
Gupta Admin <vvaibhavguptaa@gmail.com>
2026-02-07 13:08:26 +0530

    Restored Luxury Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Manifesto from './components/Manifesto';
+import ProductShowcase from './components/ProductShowcase';
+import ServicesBento from './components/ServicesBento';
+
 function App() {
   return (
-    <div style={{
-      backgroundColor: 'red',
-      height: '100vh',
-      width: '100vw',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      color: 'white',
-      fontSize: '30px'
-    }}>
-      <h1>SYSTEM ONLINE</h1>
+    <div className="bg-[#050505] min-h-screen w-full overflow-x-hidden text-white selection:bg-yellow-500 selection:text-black">
+
+      {/* Navigation */}
+      <Navbar />
+
+      {/* The Luxury Sequence */}
+      <main className="flex flex-col">
+        <Hero />
+        <Manifesto />
+        <ProductShowcase />
+        <ServicesBento />
+      </main>
+
     </div>
   );
 }
