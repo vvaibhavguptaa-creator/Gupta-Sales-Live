93f3e266ee943010b1568624d37eef73fcb69701
Gupta Admin <vvaibhavguptaa@gmail.com>
2026-02-08 17:15:18 +0530

    fjbweifb

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -7,7 +7,7 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 const SmoothScroll = () => {
     useEffect(() => {
         const lenis = new Lenis({
-            duration: 1.5,
+            duration: 2.0,
             easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
             orientation: 'vertical',
             gestureOrientation: 'vertical',
